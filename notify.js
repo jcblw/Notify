3e43c3727fe914e3a9cd78d8e0a2024331d3a683
Jacob Lowe <jacoblowe2.0@gmail.com>
2011-11-10 22:29:21 -0800

    Adding some comments and starting versioning

--- notify.js
+++ notify.js
@@ -1,12 +1,21 @@
 /*
- * Notify - A Jquery Notification Extension
- * Author: Jacob Lowe http://redeyeoperations.com
+ * Notify 0.0.1 - A Jquery Notification Extension
+ * Homepage: redeyeoperation.com/plugins/Notify
+ *
+ * Author: Jacob Lowe (redeyeoperations.com)
  * Twitter Handle @jacoblowe2dot0
-*/
-
-
-// jQuery.support.transition
-// to verify that CSS3 transition is supported (or any of its browser-specific implementations)
+ *
+ * Copyright (c) 2010 Jacob Lowe (http://redeyeoperations.com)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * 
+ * Built for jQuery library
+ * http://jquery.com
+ *
+ * jQuery.support.transition
+  *to verify that CSS3 transition is supported (or any of its browser-specific implementations)
+ */
 
 $.support.transition = (function(){ 
     var thisBody = document.body || document.documentElement,
@@ -16,10 +25,15 @@ $.support.transition = (function(){
     return support; 
 }());
 
+//Start of actual extension
+
 (function($){
+    
+    
     
     var ele, space, timer,
             modTest = $.support.transition,
+            //Show animation + classes toggle/reset
             show = (modTest) ?
                     function(element, extClass , txt){
                             element
@@ -32,6 +46,7 @@ $.support.transition = (function(){
                                 .html(txt)
                                 .animate({height: '30px'},400);
                     },
+            //Hide animation + classes toggle
             hide = (modTest) ?
                     function(element){
                             element
@@ -47,6 +62,7 @@ $.support.transition = (function(){
                                     .animate({height: '0px'},400);
                         
                     },
+            //Events object
             events = {
                     close : function(){
                         $('.notify-close').bind('click', function(){
@@ -65,6 +81,7 @@ $.support.transition = (function(){
                         
                     }
             },
+            //Build - Adding element to the document
             build = function(){
                 
                     $('body')
@@ -74,10 +91,11 @@ $.support.transition = (function(){
                     space = $('#notify-placeholder');
             
             },
+            //Were variables are parsed and order of event are defined
             handle = function(type, txt, options){
-                    
+                    //Clear timeout if there is one
                     clearTimeout(timer);    
-                
+                    //We have option
                     if(typeof (options) === 'object'){            
                             if(options.close){
                                 txt = txt + ' <div class="notify-close"></div>';
@@ -94,12 +112,12 @@ $.support.transition = (function(){
                             if(options.occupySpace){                               
                                     show(space, 'blank', '');                           
                             }
-                            
+                    //No options
                     }else{                       
                         show(ele, type, txt);                        
                     }                
             };
-
+    //Extending jquery with a commands
     $.extend({
         notify : {
             success: function(txt, options){
@@ -114,13 +132,16 @@ $.support.transition = (function(){
             basic : function(txt, options){
                 handle('basic', txt, options);
             },
+            //Only command that really does something differntly
             close : function(){
                 hide(ele);
                 hide(space);
             }
+            //TODO: Add setting function to change defaults
         }
     });
     
+    //Script about loaded let build
     build();
     
 }(jQuery));
